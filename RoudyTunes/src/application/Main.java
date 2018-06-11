ba8a691f93281c81fc5d114813f8309fd6675e0c
manueldeaguinaga <manueldeaguinaga@192.168.0.27>
2018-06-11 17:50:27 -0500

    Initial Check in

---
+++ RoudyTunes/src/application/Main.java
@@ -0,0 +1,26 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("view/PlayerView.fxml"));
+			primaryStage.setScene(new Scene(root, 500, 500));
+			primaryStage.setTitle("RowdyTunes");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
